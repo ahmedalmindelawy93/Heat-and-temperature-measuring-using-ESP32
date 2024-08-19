ae610f78fd834ed7698390b7195b71089ebb324f
unknown <ahmedalmindelawy30111993@gmail.com>
2024-08-19 23:52:47 +0300

    finishing first step code

--- Heat-and-temperature-measuring-using-ESP32/HT-ESP32/HT-ESP32.ino
+++ Heat-and-temperature-measuring-using-ESP32/HT-ESP32/HT-ESP32.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
 
+// Including the package of the sensor
+#include <DHT.h>
+DHT dht(17,DHT22);
+
+void setup() {
+  // begin to measure
+  dht.begin();
+  //delay(1000);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+// defining two variables
+// the first one is for temperature
+// and the second is for hummidity
+float Temp_value=dht.readTemperature();
+float Humidity_value=dht.readHumidity();
+
+// outputing by printing the results 
+// on the serial monitor in the IDE
+// temperature is measured with C 
+Serial.print("TEMP : ");
+Serial.print(Temp_value);
+Serial.print(" C ");
+// hummidity is measured in percentage %
+Serial.print("___ HUMIDITY : ");
+Serial.print(Humidity_value);
+Serial.print(" % ");
+Serial.println("");
+// giving some delay between two records
+delay(1000);
 
 }
